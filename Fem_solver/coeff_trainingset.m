8ecec376e11c6ebe4e4756d2651125d6b8126ba3
riccardorota <riccardo2.rota@mail.polimi.it>
2024-07-20 15:05:26 +0200

    solver matlab pronto

--- Fem_solver/coeff_trainingset.m
+++ Fem_solver/coeff_trainingset.m
@@ -6,11 +6,11 @@ y_min = 0;
 y_max = 1;
 f0 = @(x,y) 0.*x.*y + 1;
 f1 = @(x,y) sin(x).*sin(y)+1;
-f2 = @(x,y) x+y;
-f3 = @(x,y) x.*y;
+f2 = @(x,y) x+y+0.1;
+f3 = @(x,y) x.*y+0.1;
 f4 = @(x,y) exp(x+y);
 f={f0,f1,f2,f3,f4};
-nRef = [2, 2, 3, 3, 3];
-[Q_matrix] = generate_q(f, nRef, x_min, x_max, y_min, y_max, 'coefficient.txt');
+nPoints = [2, 2, 3, 3, 3];  %these are the number of suddivision of each edge, number of points will be (2^n + 1) ^2
+[Q_matrix] = generate_q(f, nPoints, x_min, x_max, y_min, y_max, 'coefficient.txt');
 
 
