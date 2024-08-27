3c356d88e35d795561cf179ca9df43309a9f1e93
riccardorota <riccardo2.rota@mail.polimi.it>
2024-08-27 10:31:16 +0200

    aggiunti commenti a 4.2

--- Fem_solver/helpme.m
+++ Fem_solver/helpme.m
@@ -6,20 +6,20 @@
 % -escludere la parte di postprocessing e calcolo degli errori per velocizzare le prestazioni
 
 %% Script utili
-% C_convergence fa un test di convergenza usando un coefficiente e una forzante specifica (vedi
-% ConvergenceTest/convergence_test_functions.m), plotta la velocità di convergenza H1 e L2 e mette gli errori in
-% una tabella. Converge tutto bene e i risultati sono salvati in ConvergenceTest.
-% 
-% coeff_trainingset permette di specificare un dominio, un vettore di funzioni e un vettore di numeri di partizioni
-% e valuta le funzioni sui punti richiesti del dominio, restituendo per ogni funzione un vettore riga.
-% L'output sarà quindi una matrice in cui ogni riga rappresenta la distribuzione di un diverso coefficiente.
-% Viene anche salvata su un file txt.
-% 
 % sol_trainingset permette, dato un file contenente su ogni riga una distribuzione del coefficiente, di calcolare le
 % rispettive soluzioni fem, e le restituisce sempre in forma di matrice e in forma di file txt. è possibile specificare
 % gli nRef desiderati per ogni soluzione.
 % Le soluzioni sono ordinate riga per riga: da (0,0) a (0,n) .... da (n,0)
 % a (n,n)
+%
+% coeff_trainingset permette di specificare un dominio, un vettore di funzioni e un vettore di numeri di partizioni
+% e valuta le funzioni sui punti richiesti del dominio, restituendo per ogni funzione un vettore riga.
+% L'output sarà quindi una matrice in cui ogni riga rappresenta la distribuzione di un diverso coefficiente.
+% Viene anche salvata su un file txt.
+%
+% C_convergence fa un test di convergenza usando un coefficiente e una forzante specifica (vedi
+% ConvergenceTest/convergence_test_functions.m), plotta la velocità di convergenza H1 e L2 e mette gli errori in
+% una tabella. Converge tutto bene e i risultati sono salvati in ConvergenceTest.
 
 %% Modifiche alle funzioni date
 % [uh, femregion, errors, solutions]=C_main2D(TestName,nRef, diff_coeff, post_proc)
